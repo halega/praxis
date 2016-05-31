50781604a40cf8b412811e7055ca767aa14dcae7
Stas Kalashnikov <stas@halega.com>
2016-05-31 07:58:31 +0300

    dup1: bufio, os.Stdin, map, if, for range map

---
+++ 2016-05-31/dup1/main.go
@@ -0,0 +1,21 @@
+// Dup1 finds and outputs duplicate lines with count of duplication.
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	input := bufio.NewScanner(os.Stdin)
+	counts := make(map[string]int)
+	for input.Scan() {
+		counts[input.Text()]++
+	}
+	for line, count := range counts {
+		if count > 1 {
+			fmt.Println(count, line)
+		}
+	}
+}
